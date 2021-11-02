7f988fc3cc7f1c6a653f405a2571e249c9e40f9a
JachymT <72783315+JachymT@users.noreply.github.com>
2021-11-02 15:18:09 +0000

    Update main.cpp

--- Code/OCR challenges/8/main.cpp
+++ Code/OCR challenges/8/main.cpp
@@ -1,51 +1,55 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
+#include <string>
 
 using namespace std;
 
 int main() 
 {
+    cout << "Enter your name:\n";
+    string name;
+    cin >> name;
     cout << "Answer these 10 arithmatic questions:\n";
+    
     int score = 0;
     srand (time(NULL));
     
     for(int i=0; i < 10; i++){
-      int num1 = (rand() % 10) + 1;         //random number between 1 and 10
-      int num2 = (rand() % 10) + 1;
-      int symbolInt = (rand() % 4) + 1;
+        int num1 = (rand() % 10) + 1;         //random number between 1 and 10
+        int num2 = (rand() % 10) + 1;
+        int symbolInt = (rand() % 4) + 1;
       
-      char symbol;
-      int sol, ans;
+        char symbol;
+        int sol, ans;
       
-      if(symbolInt == 1){
-          symbol = '+';
-          sol = num1 + num2;
-      }else if(symbolInt == 2){
-          symbol = '-';
-          sol = num1 - num2;
-      }else if(symbolInt == 3){
-          symbol = '/';
-          int childSol = num1 * num2;
-          int temp = num1;
-          num1 = childSol;
-          sol = temp;
-      }else{
-          symbol = 'x';
-          sol = num1 * num2;
-      }
+         if(symbolInt == 1){
+            symbol = '+';
+            sol = num1 + num2;
+        }else if(symbolInt == 2){
+            symbol = '-';
+            sol = num1 - num2;
+        }else if(symbolInt == 3){
+            symbol = '/';
+            int mult = num1 * num2;
+            int temp = num1;
+            num1 = mult;
+            sol = temp;
+        }else{
+            symbol = 'x';
+            sol = num1 * num2;
+        }
       
-      cout << num1 << symbol << num2 << "=";
-      cin >> ans;
+        cout << num1 << symbol << num2 << "=";
+        cin >> ans;
       
-      if(ans == sol){
-        cout << "correct\n";
-        score++;
-      } else{
-        cout << "the answer was " << sol << "\n";
-      }
+        if(ans == sol){
+            cout << "correct\n";
+            score++;
+        }else{
+            cout << "the answer was " << sol << "\n";
+        }
     }
-    
-    cout << "your score was " << score;
+    cout << name << "'s score was " << score << " out of 10";
     return 0;
 }
