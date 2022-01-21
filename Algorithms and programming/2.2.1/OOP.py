fac1e79ff02b333e9d5781461c615afa2d972788
JachymT <72783315+JachymT@users.noreply.github.com>
2022-01-21 10:12:19 +0000

    Update OOP.py

--- Algorithms and programming/2.2.1/OOP.py
+++ Algorithms and programming/2.2.1/OOP.py
@@ -4,3 +4,6 @@
 # Attributes model characeristics of an object like we would use variables or data structures in structured programming
 # Methods model behaviours of an object like we would use functions for in structured programming
 # A class defines a blueprint for these attributes and methods
+# Creating instances of a class can be done to create as many objects as needed - instantiation
+
+# Crop simulation
