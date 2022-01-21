cfe5c0b0187669a00e094b42e29768e9674bf59f
JachymT <72783315+JachymT@users.noreply.github.com>
2022-01-21 10:07:51 +0000

    Create OOP.py

---
+++ Algorithms and programming/2.2.1/OOP.py
@@ -0,0 +1,6 @@
+# Following OOP python tutorial
+# http://pythonschool.net/oop/introduction-to-object-oriented-programming/
+# Object Oriented Programming is a concept that more closely models the real world
+# Attributes model characeristics of an object like we would use variables or data structures in structured programming
+# Methods model behaviours of an object like we would use functions for in structured programming
+# A class defines a blueprint for these attributes and methods
